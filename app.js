9316a51f9422c8c39d0f975a31e8f4eb3d22720d
Tobias Kim <madeffort0903@gmail.com>
2024-06-07 17:18:57 +0900

    localstorage

---
+++ app.js
@@ -0,0 +1,30 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const USERNAME_KEY = 'username';
+const HIDDEN_CLASSNAME = 'hidden';
+
+function paintGreetings(username) {
+  greeting.innerText = username
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUserName)
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(savedUserName);
+}
