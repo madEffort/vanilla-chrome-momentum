8befd764d55da29577932916cff661d8eb8e4056
Tobias Kim <madeffort0903@gmail.com>
2024-06-07 18:16:08 +0900

    clock done .

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector('#clock')
+
+function getClock() {
+  const date = new Date();
+  hours = String(date.getHours()).padStart(2, '0');
+  minutes = String(date.getMinutes()).padStart(2, '0');
+  seconds = String(date.getSeconds()).padStart(2, '0');
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(getClock, 1000)
